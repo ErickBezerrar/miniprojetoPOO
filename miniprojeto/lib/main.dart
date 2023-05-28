c8503f9fda86ad5166f08af3da0f295605959fb1
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-27 21:43:10 -0300

    navegacao

--- miniprojeto/lib/main.dart
+++ miniprojeto/lib/main.dart
@@ -1,126 +1,141 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:miniprojeto/lib/testinho.dart';
 
-void main() => runApp(BookStoreApp());
 
-class BookStoreApp extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BookStore',
+      title: 'Meu App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BookStoreHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class BookStoreHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _BookStoreHomePageState createState() => _BookStoreHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _BookStoreHomePageState extends State<BookStoreHomePage> {
-  List<dynamic> books = [];
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
 
-  Future<void> fetchBooks() async {
-    final response = await http.get(Uri.https(
-      'www.googleapis.com',
-      '/books/v1/volumes',
-      {'q': 'romance',
-      'key': 'AIzaSyA5jvZzwUztQ4NT1c7YicLnOKTcUFQmutA'}
-    ));
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      setState(() {
-        books = data['items'];
-      });
-    } else {
-      print('Failed to fetch books');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchBooks();
-  }
+  final List<Widget> _pages = [
+    HomePageContent(),
+    CartPageContent(),
+    ProfilePageContent(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Book Store'),
+        backgroundColor: Color.fromRGBO(149, 206, 207, 1.0),
+        title: Text('Menu Inicial'),
+        leading: _currentIndex != 0
+            ? IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 0;
+                  });
+                },
+              )
+            : null,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.blue.shade200, Colors.white],
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-        ),
-        child: GridView.builder(
-          padding: EdgeInsets.all(16.0),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16.0,
-            mainAxisSpacing: 16.0,
-            childAspectRatio: 0.75,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
           ),
-          itemCount: books.length,
-          itemBuilder: (context, index) {
-            final book = books[index];
-            final title = book['volumeInfo']['title'];
-            final authors = book['volumeInfo']['authors'] != null
-                ? book['volumeInfo']['authors'].join(', ')
-                : 'Unknown';
-            final thumbnail = book['volumeInfo']['imageLinks'] != null
-                ? book['volumeInfo']['imageLinks']['thumbnail']
-                : 'https://via.placeholder.com/150';
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
 
-            return Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: CachedNetworkImage(
-                      imageUrl: thumbnail,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) =>
-                          CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                    ),
-                  ),
-                  SizedBox(height: 8.0),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(authors),
-                  ),
-                  SizedBox(height: 8.0),
-                ],
-              ),
-            );
-          },
+class HomePageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(206, 252, 252, 1.0),
+            Colors.white,
+          ],
         ),
+      ),
+      child: Center(
+        child: Text('Conteúdo da Tela Principal'),
+      ),
+    );
+  }
+}
+
+class ProfilePageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(206, 252, 252, 1.0),
+            Colors.white,
+          ],
+        ),
+      ),
+      child: Center(
+        child: Text('Esta é a tela do perfil'),
+      ),
+    );
+  }
+}
+
+class CartPageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(206, 252, 252, 1.0),
+            Colors.white,
+          ],
+        ),
+      ),
+      child: Center(
+        child: Text('Esta é a tela do carrinho de compras'),
       ),
     );
   }
