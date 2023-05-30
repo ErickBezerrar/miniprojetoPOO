253a5f748d016e66bd87fc856547271f8655a623
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-30 10:49:02 -0300

    miniprojeto -> Tela carrinho de compras

--- miniprojeto/lib/code/cartPage.dart
+++ miniprojeto/lib/code/cartPage.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class CartPageContent extends StatelessWidget {
@@ -10,13 +10,86 @@ class CartPageContent extends StatelessWidget {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            Color.fromRGBO(206, 252, 252, 1.0),
-            Colors.white,
+            Color(0xFF2068AE), // Cor personalizada 1
+            Color(0xFF4AADD6), // Cor personalizada 2
           ],
         ),
       ),
-      child: Center(
-        child: Text('Esta é a tela do carrinho de compras'),
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/book_image.png', // Substitua pelo caminho da imagem do livro
+                          width: 80,
+                          height: 80,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Nome do Livro',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'R\$ 29,90',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          // Ação do botão "Excluir"
+                        },
+                        icon: Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            margin: EdgeInsets.only(bottom: 16),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(Size(315, 66)),
+                backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(218, 109, 143, 1.0)),
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                  TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+                ),
+              ),
+              onPressed: () {
+                // Ação do botão "Fechar Pedido"
+              },
+              child: Text("Fechar Pedido"),
+            ),
+          ),
+        ],
       ),
     );
   }
